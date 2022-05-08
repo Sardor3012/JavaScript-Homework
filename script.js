348c3ff25460c7ae7c60155a90e63a6b16c811f6
Сардор Абсаматов <absamatovs7@gmail.com>
2022-05-08 20:26:03 +0500

    end of job

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-hello
+//Task 1
+let arr = [
+    {name: 'Cobalt', cost: 15000},
+    {name: 'Jentra', cost: 16000},
+    {name: 'Tico', cost: 4000},
+    {name: 'Matiz', cost: 6000},
+    {name: 'Nexia', cost: 12000},
+    {name: 'Jiguli', cost: 150}
+];
+let cheapArr = arr.filter(arr => arr.cost <= 7000);
+
+console.log(cheapArr);
+
+//Task 2
+let arr2 = [1, 2, 'hello', 24, 'world', undefined, null, 'error', 22]
+let filteredArr2 = arr2.filter(arr2 => arr2 > 0);
+
+console.log(filteredArr2);
+
+//Task 3
+let arr3 = [1, 2, 'hello', 24, 'world', undefined, null, 'error', 22]
+let arr3_length = arr3.length
+
+if (arr3_length > 5) {
+    console.log('good');
+} else {
+    console.log('bad');
+}
+
+//Task 5
+let arr5 = [1, 2, 'hello', 24, 'world', undefined, null, 'error', 22]
+let filteredArr5 = arr2.filter(arr5 => arr5 > 0);
+
+if (filteredArr5 > 5) {
+    console.log('good');
+} else {
+    console.log('bad');
+}
+
+
+
+
+
+
+
