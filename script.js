6ebb0c920113beb4b53ee3e6194471c83c30a330
Sardor <103948539+Sardor3012@users.noreply.github.com>
2022-05-12 19:12:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-//Task 1
 let arr = [
-    {name: 'Cobalt', cost: 15000},
-    {name: 'Jentra', cost: 16000},
-    {name: 'Tico', cost: 4000},
-    {name: 'Matiz', cost: 6000},
-    {name: 'Nexia', cost: 12000},
-    {name: 'Jiguli', cost: 150}
-];
-let cheapArr = arr.filter(arr => arr.cost <= 7000);
+    {
+        name: 'moloko',
+        price: 10000
+    },
+    {
+        name: 'shokolad',
+        price: 5400
+    },
+    {
+        name: 'water',
+        price: 4000
+    },
+    {
+        name: 'coca cola',
+        price: 9000
+    },
+    {
+        name: 'chocotella',
+        price: 20000
+    },
+    {
+        name: 'chips',
+        price: 17000
+    },
+    {
+        name: 'pelmeshki',
+        price: 22000
+    },
+    {
+        name: 'vodka',
+        price: 55000
+    },
+    {
+        name: 'shorts',
+        price: 50000
+    },
+    {
+        name: 'parlament',
+        price: 16000
+    },
+    {
+        name: 'aybon gold',
+        price: 10000
+    }
+]
+let ask = prompt('Что вы хотите найти?')
+let filtered = arr.filter(item => {
+    let val = arr.includes(ask)
 
-console.log(cheapArr);
-
-//Task 2
-let arr2 = [1, 2, 'hello', 24, 'world', undefined, null, 'error', 22]
-let filteredArr2 = arr2.filter(arr2 => arr2 > 0);
-
-console.log(filteredArr2);
-
-//Task 3
-let arr3 = [1, 2, 'hello', 24, 'world', undefined, null, 'error', 22]
-let filteredArr3 = arr3.filter(arr3 => arr3 > 0);
-
-if (filteredArr3 >= 5) {
-    console.log('good');
-} else {
-    console.log('bad');
-}
-
-//Task 5
-let arr5 = [1, 2, 'hello', 24, 'world', undefined, null, 'error', 22]
-let filteredArr5 = arr5.filter(arr5 => arr5 > 0);
-
-if (filteredArr5 > 5) {
-    console.log('good');
-} else {
-    console.log('bad');
-}
-
-
-
-
-
-
-
+    if (val === arr.name) {
+        alert(arr.name)
+    } else {
+        alert('По вашему запросу ничего не найдено')
+    }
+})
 
