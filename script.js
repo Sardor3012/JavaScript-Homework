b38e99c3733095b9807aaa6a03d603ebf934fe7b
Sardor <103948539+Sardor3012@users.noreply.github.com>
2022-06-01 17:09:35 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let arr = [12, 12, 'hello', true, {}, null, 'str']
+// function min(num1, num2) {
+//     Math.min(num1, num2)
+// }
+// min(1, 2)
 
-let categories = [
-    {
-        type: "string",
-        count: 0
-    },
-    {
-        type: "number",
-        count: 0
-    },
-    {
-        type: "boolean",
-        count: 0
-    },
-    {
-        type: "object",
-        count: 0
-    },
-]
+// function min2([num1, num2]) {
+//     Math.min([num1, num2])
+// }
+// min2([1, 2])
 
-for(let item of arr) {
-    for(let item2 of categories) {
-        if (item === item2.type) {
-            item2.count++
-        }
-    }
+let name = 'Sardor'
+let surname = 'Absamatov'
+let age = 13
+let school = 35
+
+function GetRandomID(n,s,a,sc) {
+    let ids = [
+        `${n}${a}${sc}`, 
+        `${a}${n}${s}`,
+        `${a}${n}${sc}`,
+        `${n}${s}${a}${sc}` 
+    ]
+    let rnd = Math.round(Math.random() * ids.length)
+
+    console.log(ids[rnd]);
 }
 
-console.log(categories);
+GetRandomID(name, surname, age, school)
+
+
