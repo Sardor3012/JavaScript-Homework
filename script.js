30089a2d74750ff66d634176df0719100ce21bf6
Sardor <103948539+Sardor3012@users.noreply.github.com>
2022-06-22 14:33:33 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,68 @@
-// function min(num1, num2) {
-//     Math.min(num1, num2)
-// }
-// min(1, 2)
+let button = document.querySelector('.btn-model-1')
+let button2 = document.querySelector('.btn-model-2')
+let iphoneimg = document.querySelector('.iphone-img') 
+let iphone134 = document.querySelector('.iphone-13-4')
+let iphone134max = document.querySelector('.iphone-13-4-max')
+let active = document.querySelector('.active')
+let colorbuttons = document.querySelector('.colour-buttons')
+let colorbtn1 = document.querySelector('.color-btn')
+let colorbtn2 = document.querySelector('.color-btn2')
+let colorbtn3 = document.querySelector('.color-btn3')
+let colorbtn4 = document.querySelector('.color-btn4')
+let colorbtn5 = document.querySelector('.color-btn5')
+let progreen13 = document.querySelector('.pro13-green')
+let prosilver13 = document.querySelector('.pro13-silver')
+let progold13 = document.querySelector('.pro13-gold')
+let prographite13 = document.querySelector('.pro13-graphite')
+let problue13 = document.querySelector('.pro13-blue')
 
-// function min2([num1, num2]) {
-//     Math.min([num1, num2])
-// }
-// min2([1, 2])
-
-let name = 'Sardor'
-let surname = 'Absamatov'
-let age = 13
-let school = 35
-
-function GetRandomID(n,s,a,sc) {
-    let ids = [
-        `${n}${a}${sc}`, 
-        `${a}${n}${s}`,
-        `${a}${n}${sc}`,
-        `${n}${s}${a}${sc}` 
-    ]
-    let rnd = Math.round(Math.random() * ids.length)
-
-    console.log(ids[rnd]);
+button.onclick = () =>{
+    iphoneimg.style.display = 'none'
+    iphone134max.style.display = 'none'
+    iphone134.style.display = 'flex'
 }
-
-GetRandomID(name, surname, age, school)
-
-
+button2.onclick = () =>{
+    iphoneimg.style.display = 'none'
+    iphone134.style.display = 'none'
+    iphone134max.style.display = 'flex'
+}
+colorbtn1.onclick = () =>{
+    iphoneimg.style.display = 'none'
+    iphone134.style.display = 'none'
+    iphone134max.style.display = 'none'
+    progreen13.style.display = 'flex'
+}
+colorbtn2.onclick = () =>{
+    iphoneimg.style.display = 'none'
+    iphone134.style.display = 'none'
+    iphone134max.style.display = 'none'
+    progreen13.style.display = 'none'
+    prosilver13.style.display = 'flex'
+}
+colorbtn3.onclick = () =>{
+    iphoneimg.style.display = 'none'
+    iphone134.style.display = 'none'
+    iphone134max.style.display = 'none'
+    progreen13.style.display = 'none'
+    prosilver13.style.display = 'none'
+    progold13.style.display = 'flex'
+}
+colorbtn4.onclick = () =>{
+    iphoneimg.style.display = 'none'
+    iphone134.style.display = 'none'
+    iphone134max.style.display = 'none'
+    progreen13.style.display = 'none'
+    prosilver13.style.display = 'none'
+    progold13.style.display = 'none'
+    prographite13.style.display = 'flex'
+}
+colorbtn5.onclick = () =>{
+    iphoneimg.style.display = 'none'
+    iphone134.style.display = 'none'
+    iphone134max.style.display = 'none'
+    progreen13.style.display = 'none'
+    prosilver13.style.display = 'none'
+    progold13.style.display = 'none'
+    prographite13.style.display = 'none'
+    problue13.style.display = 'flex'
+}
